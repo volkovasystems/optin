59be49dfd970c363d1a7f166fff9b884ccd70ce0
volkovasystems <richeve.bebedor@gmail.com>
2016-11-01 05:59:30 +0800

    Save major changes.

--- optin.js
+++ optin.js
@@ -37,7 +37,6 @@
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/optin.git",
 			"global": true,
-			"class": true
 		}
 	@end-module-configuration
 
@@ -49,11 +48,36 @@
 
 	@include:
 		{
+			"doubt": "doubt",
+			"falzy": "falzy",
 			"harden": "harden",
-			"lilfy": "lilfy"
+			"lilfy": "lilfy",
+			"truly": "truly",
+			"truu": "truu"
 		}
 	@end-include
 */
+
+if( typeof require == "function" ){
+	var doubt = require( "doubt" );
+	var falzy = require( "falzy" );
+	var harden = require( "harden" );
+	var lilfy = require( "lilfy" );
+	var truly = require( "truly" );
+	var truu = require( "truu" );
+}
+
+if( typeof window != "undefined" &&
+	!( "doubt" in window ) )
+{
+	throw new Error( "doubt is not defined" );
+}
+
+if( typeof window != "undefined" &&
+	!( "falzy" in window ) )
+{
+	throw new Error( "falzy is not defined" );
+}
 
 if( typeof window != "undefined" &&
 	!( "harden" in window ) )
@@ -65,6 +89,18 @@ if( typeof window != "undefined" &&
 	!( "lilfy" in window ) )
 {
 	throw new Error( "lilfy is not defined" );
+}
+
+if( typeof window != "undefined" &&
+	!( "truly" in window ) )
+{
+	throw new Error( "truly is not defined" );
+}
+
+if( typeof window != "undefined" &&
+	!( "truu" in window ) )
+{
+	throw new Error( "truu is not defined" );
 }
 
 harden( "STYLE", "style" );
@@ -86,7 +122,7 @@ harden( "SCRIPT", "script" );
 			"file": "string",
 			"basePath": "string",
 
-			"dependency": "Array"
+			"dependency": "[ 'object' ]"
 		}
 	@end-option
 */
@@ -94,21 +130,36 @@ var optin = function optin( option ){
 	/*;
 		@meta-configuration:
 			{
-				"option:required": "object"
+				"option:required": [
+					"object",
+					"string"
+				]
 			}
 		@end-meta-configuration
 	*/
+
+	if( typeof option == "string" ){
+		if( typeof module == "object" && typeof module.require == "function" ){
+			return module.require( option );
+
+		}else{
+			let token = option;
+			let name = token.match( optin.MODULE_NAME_PATTERN )[ 1 ];
+			let file = token.match( optin.MODULE_FILE_PATTERN );
+
+			option = { "name": name, "file": file };
+		}
+	}
+
 	if( !optin.BOOTED ){
 		throw new Error( "optin is not loaded properly" );
 	}
 
 	option = option || { };
 
-	var dependency = option.dependency;
-	if( dependency &&
-		Array.isArray( dependency ) )
-	{
-		var element = [ ];
+	let dependency = option.dependency;
+	if( truu( dependency ) && doubt( dependency ).ARRAY ){
+		let element = [ ];
 
 		dependency = dependency.reverse( );
 
@@ -119,12 +170,8 @@ var optin = function optin( option ){
 		return element;
 	}
 
-	var pointer = option.pointer;
-	var file = option.file;
-	var basePath = option.basePath;
-
-	var name = option.name;
-	if( !name ){
+	let name = option.name;
+	if( falzy( name ) ){
 		throw new Error( "name not specified" );
 	}
 
@@ -134,39 +181,41 @@ var optin = function optin( option ){
 		return optin.data.dependency[ name ].element;
 	}
 
-	var path = option.path;
+	let basePath = option.basePath ||
+		window.BASE_LIBRARY_PATH ||
+		optin.DEFAULT_BASE_PATH;
 
-	if( !path && name && file ){
-		basePath = basePath ||
-			window.BASE_LIBRARY_PATH ||
-			"/library";
+	let file = option.file;
 
+	let path = option.path;
+
+	if( falzy( path ) && truly( name ) && truly( file ) ){
 		path = [ basePath, name, file ].join( "/" );
 	}
 
-	if( !path ){
+	if( falzy( path ) ){
 		throw new Error( "path not specified" );
 	}
 
-	var type = option.type;
-	if( !type ){
-		if( ( /(\.css|\.scss|\.less|\.style)$/ ).test( path ) ){
+	let type = option.type;
+	if( falzy( type ) ){
+		if( optin.STYLE_EXTENSION_PATTERN.test( path ) ){
 			option.type = STYLE;
 
-		}else if( ( /(\.js|\.jsx|\.script)$/ ).test( path ) ){
+		}else if( optin.SCRIPT_EXTENSION_PATTERN.test( path ) ){
 			option.type = SCRIPT;
 		}
 	}
 
 	type = option.type;
 
-	if( !type ){
+	if( falzy( type ) ){
 		throw new Error( "unknown dependency type" );
 	}
 
 	optin.data.dependency[ option.name ] = option;
 
-	var element = null;
+	let element = null;
 	if( type == STYLE ){
 		element = document.createElement( "link" );
 		element.setAttribute( "class", STYLE );
@@ -183,11 +232,11 @@ var optin = function optin( option ){
 
 	element.setAttribute( "name", name );
 
-	var version = option.version;
-	if( version ){
+	let version = option.version;
+	if( truly( version ) ){
 		element.setAttribute( "version", version );
 
-		if( ( /\?/ ).test( path ) ){
+		if( optin.QUERY_SEPARATOR_PATTERN.test( path ) ){
 			path = path + "&version=" + version;
 
 		}else{
@@ -195,9 +244,10 @@ var optin = function optin( option ){
 		}
 	}
 
-	if( pointer ){
-		if( ( /\?/ ).test( path ) ){
-			path = path + "&pointer=" + lilfy( pointer );
+	let pointer = option.pointer;
+	if( truly( pointer ) ){
+		if( optin.QUERY_SEPARATOR_PATTERN.test( path ) ){
+			path = `${ path }&pointer=${ lilfy( pointer ) }`;
 
 		}else{
 			path = "?pointer=" + lilfy( pointer );
@@ -213,27 +263,22 @@ var optin = function optin( option ){
 		element.setAttribute( "type", "application/javascript" );
 	}
 
-	var callback = option.callback;
-	if( !callback &&
-		( /callback\=[a-zA-Z][a-zA-Z0-9_$]+?/ ).test( path ) )
-	{
-		callback = ( path.match( /callback\=([a-zA-Z][a-zA-Z0-9_$]+?)/ ) || [ ] )[ 1 ];
+	let callback = option.callback;
+	if( falzy( callback ) && optin.CALLBACK_PATTERN.test( path ) ){
+		callback = path.match( optin.CALLBACK_NAME_PATTERN )[ 1 ];
 	}
 
-	if( callback &&
-		!( /callback\=[a-zA-Z][a-zA-Z0-9_$]+?/ ).test( path ) )
-	{
-		if( ( /\?/ ).test( path ) ){
-			path = path + "&callback=" + callback;
+	if( truly( callback ) && !optin.CALLBACK_PATTERN.test( path ) ){
+		if( optin.QUERY_SEPARATOR_PATTERN.test( path ) ){
+			path = `${ path }&callback=${ callback }`;
 
 		}else{
-			path = "?callback=" + callback;
+			path = `?callback=${ callback }`;
 		}
 	}
 
-	if( callback ){
-		element.setAttribute( "onload",
-			"optin.done( this, @callback );".replace( "@callback", callback ) );
+	if( truly( callback ) ){
+		element.setAttribute( "onload", `optin.done( this, ${ callback } );` );
 
 	}else{
 		element.setAttribute( "onload", "optin.done( this );" );
@@ -285,13 +330,13 @@ harden( "load",
 			return;
 		}
 
-		var element = optin.queue.pop( );
+		let element = optin.queue.pop( );
 
 		if( !element ){
-			var dependencyList = Object.keys( optin.data.dependency );
+			let dependencyList = Object.keys( optin.data.dependency );
 
-			var totalDependency = dependencyList.length;
-			var loadedDependency = dependencyList
+			let totalDependency = dependencyList.length;
+			let loadedDependency = dependencyList
 				.filter( function onEachDependency( name ){
 					var data = optin.data.dependency[ name ];
 
@@ -300,30 +345,32 @@ harden( "load",
 
 			dependencyList = dependencyList
 				.map( function onEachDependency( name ){
-					var data = optin.data.dependency[ name ];
+					let data = optin.data.dependency[ name ];
 
-					var duration = ( data.timeEnd || data.timeStart ) - data.timeStart;
+					let duration = ( data.timeEnd || data.timeStart ) - data.timeStart;
 					data.duration = duration;
 
-					console.log( name, "@", data.path, data.status, duration, "ms"  );
+					let path = data.path;
+					let status = data.status;
+					console.log( `${ name }@${ path }:${ status }-${ duration }ms` );
 
 					return data;
 				} );
 
-			var averageDuration = ( dependencyList
+			let averageDuration = ( dependencyList
 				.reduce( function onEachDependency( total, data ){
-					return total + data.duration;
+					return total + ( data.duration || 0 );
 				}, 0 ) / loadedDependency ).toFixed( 2 );
 
 			console.log( "all dependency loaded",
-				[ "(", loadedDependency, "/", totalDependency, ")" ].join( "" ),
-				averageDuration, "ms" );
+				`(${ loadedDependency }/${ totalDependency })`
+				`${ averageDuration }ms` );
 
 			return;
 		}
 
-		var type = element.getAttribute( "class" );
-		var name = element.getAttribute( "name" );
+		let type = element.getAttribute( "class" );
+		let name = element.getAttribute( "name" );
 
 		optin.data.current = name;
 
@@ -342,9 +389,9 @@ harden( "load",
 harden( "done",
 	function done( self, callback ){
 		if( self.getAttribute( "name" ) == optin.data.current ){
-			var name = optin.data.current;
+			let name = optin.data.current;
 
-			var data = optin.data.dependency[ name ];
+			let data = optin.data.dependency[ name ];
 
 			data.timeEnd = Date.now( );
 			data.status = "success";
@@ -366,7 +413,7 @@ harden( "done",
 harden( "failed",
 	function failed( self ){
 		if( self.getAttribute( "name" ) == optin.data.current ){
-			var name = optin.data.current;
+			let name = optin.data.current;
 
 			console.debug( "failed loading", name, self );
 
@@ -379,3 +426,19 @@ harden( "failed",
 
 		return optin;
 	}, optin );
+
+harden.bind( optin )( "DEFAULT_BASE_PATH", "/library" );
+
+harden.bind( optin )( "STYLE_EXTENSION_PATTERN", /(\.css|\.scss|\.less|\.style)$/ );
+
+harden.bind( optin )( "SCRIPT_EXTENSION_PATTERN", /(\.js|\.jsx|\.script)$/ );
+
+harden.bind( optin )( "MODULE_NAME_PATTERN", /([a-zA-Z0-9\-\_\.]+?)(?:\.[a-z]+)?$/ );
+
+harden.bind( optin )( "MODULE_FILE_PATTERN", /[a-zA-Z0-9\-\_\.]+$/ );
+
+harden.bind( optin )( "CALLBACK_PATTERN", /callback\=[a-zA-Z][a-zA-Z0-9_$]+?/ );
+
+harden.bind( optin )( "CALLBACK_NAME_PATTERN", /callback\=([a-zA-Z][a-zA-Z0-9_$]+?)/ );
+
+harden.bind( optin )( "QUERY_SEPARATOR_PATTERN", /\?/ );
